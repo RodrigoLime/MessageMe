e7315371abe9204e475c88efe48e08688a05c320
Rodrigo <rodrigo.machadoassis14@gmail.com>
2026-07-21 21:19:29 -0300

    Authentication navigation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,20 +4,20 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
+    user = User.find_by(username: params[:session][:username])
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      flash[:notice] = "Logged in successfully"
-      redirect_to user
+      flash[:success] = "Logged in successfully"
+      redirect_to root_path
     else
-      flash.now[:alert] = "There was something wrong with your login details"
+      flash.now[:error] = "There was something wrong with your login information"
       render 'new'
     end
   end
 
   def destroy
     session[:user_id] = nil
-    flash[:notice] = "Logged out"
-    redirect_to root_path
+    flash[:success] = "You have successfully logged out"
+    redirect_to login_path
   end
 end
